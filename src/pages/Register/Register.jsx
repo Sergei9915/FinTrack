28283d28a4cc88a9a949665b5aebb0930627f2be
Serhii Kapitan <serejkakaapitan@gmail.com>
2025-10-05 20:34:24 +0300

    design-signup

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import styles from './register.module.css';
+
+export const Register = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = e => {
+    e.preventDefault();
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.centerBlock}>
+      <h1 className={styles.brandTitle}>FinTrack</h1>
+      <div className={styles.registerBlock}>
+        <form action="#" onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            autoComplete="name"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            autoComplete="email"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <button type="submit">Sig In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,5 +2,5 @@ export { Dashboard } from './Dashboard';
 export { Stats } from './Stats';
 export { Categories } from './Categories';
 export { Settings } from './Settings';
-export { Login } from './Login';
-export { Register } from './Register';
+export { Login } from './Login/Login';
+export { Register } from './Register/Register';
